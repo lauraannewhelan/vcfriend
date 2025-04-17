ef852e4069a2c66012685b3420bfe72679ae065f
Laura Whelan <47507603+lauraannewhelan@users.noreply.github.com>
2025-04-17 16:58:05 +0100

    starting to work with front end

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FamilyExplorer from './components/FamilyExplorer';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>VCFriend</h1>
+        <FamilyExplorer />
+      </div>
+  );
+}
+
+export default App;
