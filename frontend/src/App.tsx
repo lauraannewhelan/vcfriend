2f22890f8b85bc8557267a2fc9a5a71f79d42cbe
Laura Whelan <47507603+lauraannewhelan@users.noreply.github.com>
2025-04-23 20:55:05 +0100

    Beacon implemented, pedigree search working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PedigreeSearch from './pages/PedigreeSearch';  // Add the new route
+import Home from './pages/Home'
 import IndividualVariants from './pages/IndividualVariants';  // Existing pages
 import UploadVCF from './pages/UploadVCF';
 import VariantBrowser from './pages/VariantBrowser';
 import Shortlist from './pages/Shortlist';
-
 function App() {
     return (
         <Router>
@@ -15,7 +13,6 @@ function App() {
                 <Route path="/upload" element={<UploadVCF />} />
                 <Route path="/variants" element={<VariantBrowser />} />
                 <Route path="/shortlist" element={<Shortlist />} />
-                <Route path="/pedigree-search" element={<PedigreeSearch />} />  {/* New route for pedigree search */}
                 <Route path="/individuals/:individualId" element={<IndividualVariants />} />
             </Routes>
         </Router>
